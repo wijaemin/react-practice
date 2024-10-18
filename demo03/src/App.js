227913f67ea0f6cd0d103a9dcb350d89c44b4037
wijaemin <minialp2002@naver.com>
2024-10-18 16:56:38 +0900

    예제3 금액선택

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -1,5 +1,6 @@
 import Exam01 from "./components/Exam01";
 import Exam02 from "./components/Exam02";
+import Exam03 from "./components/Exam03";
 function App() {
   return (
     <>
@@ -8,6 +9,8 @@ function App() {
       <Exam01/>
       <hr/>
       <Exam02/>
+      <hr/>
+      <Exam03/>
       <hr/>
     </>
 
