e13b44fe9c72ef742bdc8e63ea5ca4f367c457ff
wijaemin <minialp2002@naver.com>
2024-10-18 09:52:34 +0900

    bootstrap, bootswatch 추가

--- demo03/src/index.js
+++ demo03/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//link 대신 import를 통해 설치한 라이브러리의 CSS를 불러오도록 처리
+//node_modules에 설치한 요소들은 바로 이름을 사용하여 접근 가능
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/journal/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
