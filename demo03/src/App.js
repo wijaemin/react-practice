4c61a0e95a05b6c2587a9de1f0b4db79afdc1103
wijaemin <minialp2002@naver.com>
2024-10-19 17:43:20 +0900

    useEffect 사용해서 자기소개서 양식 만들기

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -1,6 +1,7 @@
 import Exam01 from "./components/Exam01";
 import Exam02 from "./components/Exam02";
 import Exam03 from "./components/Exam03";
+import Exam04 from "./components/Exam04";
 function App() {
   return (
     <>
@@ -11,6 +12,8 @@ function App() {
       <Exam02/>
       <hr/>
       <Exam03/>
+      <hr/>
+      <Exam04/>
       <hr/>
     </>
 
