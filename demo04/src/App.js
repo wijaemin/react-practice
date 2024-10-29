9fd7af322f74e00f24b1055136bd7eb58b3bcf1a
wijaemin <minialp2002@naver.com>
2024-10-29 23:51:45 +0900

    일정관리 프로그램

--- demo04/src/App.js
+++ demo04/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Jumbotron from "./components/Jumbotron";
+import { FaXmark } from "react-icons/fa6";
+import {useState} from "react";
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    {no:1, title:"학원가기", type:"공부"},
+    {no:2, title:"영어단어외우기", type:"공부"},
+    {no:3, title:"헬스장가기", type:"운동"},
+    {no:4, title:"친구만나기", type:"일상"}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid my-5">
+      <div className="row">
+          <div className="col-md-10 offset-md-1">
+
+            {/* 점보트론을 만들면서 제목과 내용을 전달*/}
+            <Jumbotron title="일정관리 프로그램" content="반갑습니다"/>
+            {/* 입력 화면 */}
+
+
+            {/* 출력 화면 */}
+            <div className="row mt-4">
+              {todoList.map(todo=>(
+              <div className="col-12 fs-3">
+                <span className="badge bg-primary me-2">
+                  {todo.type}
+                </span>
+                {todo.title}
+                <FaXmark className="text-danger"/>
+              </div>
+              ))};
+            </div>
+
+          </div>
+      </div>
     </div>
   );
 }
---
+++ demo04/src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+//컴포넌트 함수의 매개변수에 props를 적으면 전달되는 속성을 읽을 수 있다
+//이를 통해 상위 화면에서 전달되는 데이터를 이용한 프로그래밍이 가능
+const Jumbotron = (props)=>{
+
+    return(
+        <div className="p-4 text-light bg-dark rounded">
+        <h1>{props.title}</h1>
+        <h1>{props.content}</h1>
+        </div>
+    );
+
+};
+
+export default Jumbotron;
