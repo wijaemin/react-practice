2b314e1ffd61cabf0efeb8a15e7d2ccbbe8a0285
wijaemin <minialp2002@naver.com>
2024-10-30 23:51:10 +0900

    수정까지 완료

--- demo04/src/App.js
+++ demo04/src/App.js
@@ -2,9 +2,11 @@ import Jumbotron from "./components/Jumbotron";
 import { FaXmark } from "react-icons/fa6";
 import { FaRegEdit } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
-import {useState} from "react";
+import {useRef, useState} from "react";
+import { Modal } from "bootstrap";
 function App() {
 
+  //목록을 위한 state
   const [todoList, setTodoList] = useState([
     {no:1, title:"학원가기", type:"공부"},
     {no:2, title:"영어단어외우기", type:"공부"},
@@ -12,10 +14,19 @@ function App() {
     {no:4, title:"친구만나기", type:"일상"}
   ]);
 
+  //등록을 위한 state
   const [data, setData] = useState({
     title:"", 
     type:""
   });
+
+  //수정을 위한 state
+  const[editData, setEditData] = useState({
+    title:"", 
+    type:""
+  });
+
+  const bsModal = useRef()
 
   const changeData =(e)=>{
     setData({
@@ -31,7 +42,7 @@ function App() {
     //내용 검사 코드 추가 if(맘에 안들면 ) return;
     if(data.title.length === 0 || data.type.length === 0) return;
     const last=todoList.length-1;
-    const no = todoList.length == 0 ? 1 : todoList[last].no +1;
+    const no = todoList.length === 0 ? 1 : todoList[last].no +1;
 
     setTodoList([
       ...todoList, 
@@ -43,11 +54,56 @@ function App() {
     ]);
     setData({title:"",type:""});
   };
-
+  //todo 삭제
   const deleteTodoList = (todo) => {
     const newTodoList = todoList.filter(t=> t.no !==todo.no);
     setTodoList(newTodoList);
   };
+
+  //todo 수정을 위한 선택
+  const editTodoList = (todo) =>{
+    // setEditData(todo);//안되는 코드(얕은 복사, shallow copy)
+    setEditData({...todo});//가능한 코드 (깊은 복사, deep copy)
+
+    openModal();
+    
+  };
+  const changeEditData = e=>{
+    setEditData({
+      ...editData,
+      [e.target.name] : e.target.value
+    });
+  };
+  
+  const clearEditData = ()=>{
+    setEditData({title:"",type:""});
+    closeModal();
+  };
+
+  const saveTodoList = ()=>{
+    //editData의 내용을 todoList에 반영하고 초기화
+    const newTodoList = todoList.map(t=>{
+      if(t.no ===editData.no){
+        return {...editData};
+      }
+      return t;
+    });
+
+    setTodoList(newTodoList);
+    clearEditData();
+    closeModal();
+  };
+
+  //Modal 제어 함수
+  const openModal = ()=>{
+    const modal = new Modal(bsModal.current);
+    modal.show();
+  };
+  const closeModal = ()=>{
+    const modal =Modal.getInstance(bsModal.current);
+    modal.hide();
+  };
+
   return (
     <div className="container-fluid my-5">
       <div className="row">
@@ -78,6 +134,8 @@ function App() {
               </div>
             </div>
 
+            
+
             {/* 출력 화면 */}
             <div className="row mt-4">
               {todoList.map(todo=>(
@@ -86,7 +144,9 @@ function App() {
                   {todo.type}
                 </span>
                 {todo.title}
-                <FaRegEdit className="text-warning ms-1"/>
+                {/* 수정버튼 */}
+                <FaRegEdit className="text-warning ms-1" onClick={e=>editTodoList(todo)}/>
+                {/* 삭제버튼 */}
                 <FaXmark className="text-danger" onClick={e=>deleteTodoList(todo)}/>
               </div>
               ))}
@@ -94,6 +154,51 @@ function App() {
 
           </div>
       </div>
+
+      <div className="modal fade" ref={bsModal} id="exampleModal" 
+                   data-bs-backdrop="static" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">일정 변경</h5>
+                      <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+                  <div className="modal-body">
+                    {/* 수정 화면 */}
+                    <div className="row mt-4">
+                      <div className="col">
+                        <label className="form-label">제목</label>
+                        <input type="text" name="title" value={editData.title} 
+                              className="form-control" onChange={changeEditData}/>
+                      </div>
+                    </div>
+
+                    <div className="row mt-4">
+                    <div className="col">
+                        <label className="form-label">종류</label>
+                        <select name="type" value={editData.type} 
+                                className="form-select"onChange={changeEditData}>
+                          <option>일상</option>
+                          <option>약속</option>
+                          <option>취미</option>
+                          <option>공부</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="modal-footer">
+                      <button className="btn btn-secondary" onClick={clearEditData}>취소</button>
+                      <button className="btn btn-success" onClick={saveTodoList}>저장</button>
+                  </div>
+                </div>
+            </div>
+            </div>
+
+
+
+
     </div>
   );
 }
