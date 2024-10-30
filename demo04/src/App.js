1cff93ef9ade1f420c84d9dfb288058794e3f99b
wijaemin <minialp2002@naver.com>
2024-10-30 22:59:21 +0900

    등록 삭제

--- demo04/src/App.js
+++ demo04/src/App.js
@@ -1,5 +1,7 @@
 import Jumbotron from "./components/Jumbotron";
 import { FaXmark } from "react-icons/fa6";
+import { FaRegEdit } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
 import {useState} from "react";
 function App() {
 
@@ -10,6 +12,42 @@ function App() {
     {no:4, title:"친구만나기", type:"일상"}
   ]);
 
+  const [data, setData] = useState({
+    title:"", 
+    type:""
+  });
+
+  const changeData =(e)=>{
+    setData({
+      ...data,
+      [e.target.name] : e.target.value
+    }
+    )
+  };
+
+  const addTodoList = ()=>{
+    //data의 내용을 todoList에 추가 후 data를 초기화
+
+    //내용 검사 코드 추가 if(맘에 안들면 ) return;
+    if(data.title.length === 0 || data.type.length === 0) return;
+    const last=todoList.length-1;
+    const no = todoList.length == 0 ? 1 : todoList[last].no +1;
+
+    setTodoList([
+      ...todoList, 
+      {
+        ...data, 
+        no : no
+
+      }
+    ]);
+    setData({title:"",type:""});
+  };
+
+  const deleteTodoList = (todo) => {
+    const newTodoList = todoList.filter(t=> t.no !==todo.no);
+    setTodoList(newTodoList);
+  };
   return (
     <div className="container-fluid my-5">
       <div className="row">
@@ -18,7 +56,27 @@ function App() {
             {/* 점보트론을 만들면서 제목과 내용을 전달*/}
             <Jumbotron title="일정관리 프로그램" content="반갑습니다"/>
             {/* 입력 화면 */}
-
+            <div className="row mt-4">
+              <div className="col-6">
+                <input className="form-control" name="title" value={data.title} 
+                      onChange={changeData}/>
+              </div>
+              <div className="col-3">
+                <select className="form-select" name="type" value={data.type} 
+                      onChange={changeData}>
+                  <option value="">선택</option>
+                  <option>일상</option>
+                  <option>약속</option>
+                  <option>취미</option>
+                  <option>공부</option>
+                </select>
+              </div>
+              <div className="col-3">
+                <button className="btn btn-success w-100" onClick={addTodoList}>
+                  <FaPlus/> 추가
+                </button>
+              </div>
+            </div>
 
             {/* 출력 화면 */}
             <div className="row mt-4">
@@ -28,9 +86,10 @@ function App() {
                   {todo.type}
                 </span>
                 {todo.title}
-                <FaXmark className="text-danger"/>
+                <FaRegEdit className="text-warning ms-1"/>
+                <FaXmark className="text-danger" onClick={e=>deleteTodoList(todo)}/>
               </div>
-              ))};
+              ))}
             </div>
 
           </div>
